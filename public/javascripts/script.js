d9a942d635284b8dbe4a43f6dd11a41f6f7fae24
Paula Filipe <pac.tic.es@gmail.com>
2019-05-30 16:26:35 +0200

    calendar showing workouts and noticing clicks on them

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -13,17 +13,46 @@ window.onload = function () {
   // document.addEventListener('DOMContentLoaded', function () {
   var calendarEl = document.getElementById('calendar');
   console.log(calendar)
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: ['dayGrid', 'timegrid', 'list', 'timeline', 'rrule'],
-    defaultView: 'dayGridWeek',
-    defaultDate: '2019-05-07',
+  const calendarDOM = new FullCalendar.Calendar(calendarEl, {
+    plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'rrule'],
+    defaultView: 'timeGridWeek',
+    eventClick: function (info) {
+      var eventObj = info.event;
+
+      if (eventObj.url) {
+        alert(
+          'Clicked ' + eventObj.title + '.\n' +
+          'Will open ' + eventObj.url + ' in a new tab'
+        );
+
+        window.open(eventObj.url);
+
+        info.jsEvent.preventDefault(); // prevents browser from following link in current tab.
+      } else {
+        alert('Clicked ' + eventObj.title);
+      }
+    },
     header: {
       left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay'
     },
-  });
+    events: {
+      url: '/workouts/api',
+      method: 'GET',
+      // extraParams: {
+      //   custom_param1: 'something',
+      //   custom_param2: 'somethingelse'
+      // },
+      failure: function () {
+        alert('there was an error while fetching events!');
+      },
+      color: '#3ab6e2', // a non-ajax option
+      textColor: 'black' // a non-ajax option
+    },
 
-  calendar.render();
+  });
+  console.log(calendarDOM)
+  calendarDOM.render();
   // });
 }
